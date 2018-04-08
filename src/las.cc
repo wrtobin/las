a0a95ab8226fbec330fbbcc9b7ffb98e7e0fe0bd
William Tobin <tobinw2@rpi.edu>
2018-04-08 13:51:37 -0400

    correcting the lasComm.h changes so that even if we don't compile with MPI the MPI_Comm type is defined, to make interface specification easier, also adding return values to las.cc functions and including the debug header

--- src/las.cc
+++ src/las.cc
@@ -1,13 +1,16 @@
 #include "las.h"
+#include "lasDebug.h"
 #include <iostream>
 namespace las
 {
-  Vec * createRHS(Mat * m)
+  Vec * createRHS(Mat*)
   {
     DBG(std::cerr << "[LAS] : createRHS(Mat*) not implemented for configured backend" << std::endl);
+    return nullptr;
   }
-  Vec * createLHS(Mat * m)
+  Vec * createLHS(Mat*)
   {
     DBG(std::cerr << "[LAS] : createLHS(Mat*) not implemented for configured backend" << std::endl);
+    return nullptr;
   }
 }
