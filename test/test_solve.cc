75465cc2be404f20137413afac8a71eede173053
Jacob Merson <mersoj@rpi.edu>
2018-09-17 16:40:07 -0400

    Multiple Backend Solver Example
    closes #57

---
+++ test/test_solve.cc
@@ -0,0 +1,74 @@
+#include <las.h>
+#include <lasPETSc.h>
+#include <lasSparskit.h>
+#include <mpi.h>
+#include <cassert>
+#include <iostream>
+#include <utility>
+#include <vector>
+int main(int argc, char * argv[])
+{
+  MPI_Init(&argc, &argv);
+#ifdef USING_PETSC
+  las::initPETScLAS(&argc, &argv, LAS_COMM_WORLD);
+#endif
+  auto ops = las::getLASOps<las::BACKEND>();
+  auto vb = las::getVecBuilder<las::BACKEND>(0);
+  las::Vec * v = vb->create(3, 1, LAS_COMM_WORLD);
+  las::Vec * u = vb->create(3, 1, LAS_COMM_WORLD);
+  ops->zero(v);
+  ops->zero(u);
+  double vals[] = {1.0, 2.0, 3.0};
+  int rws[] = {0, 1, 2};
+  ops->assemble(v, 3, &rws[0], &vals[0]);
+  auto mb = las::getMatBuilder<las::BACKEND>(0);
+#if defined USING_SPARSKIT
+  las::CSRBuilder csrBldr(3, 3);
+  for (int i = 0; i < 3; ++i)
+  {
+    csrBldr.add(rws[i], rws[i]);
+  }
+  csrBldr.add(rws[0], rws[1]);
+  las::Sparsity * sprs = csrBldr.finalize();
+#elif defined USING_PETSC
+  las::Sparsity * sprs = nullptr;
+#endif
+  las::Mat * m = mb->create(3, 1, sprs, MPI_COMM_SELF);
+  ops->assemble(m, 1, &rws[0], 1, &rws[0], &vals[0]);
+  ops->assemble(m, 1, &rws[1], 1, &rws[1], &vals[1]);
+  ops->assemble(m, 1, &rws[2], 1, &rws[2], &vals[2]);
+  ops->assemble(m, 1, &rws[0], 1, &rws[1], &vals[2]);
+#if defined USING_SPARSKIT
+  las::printSparskitMat(std::cout, m, las::PrintType::full);
+  int nnz = ((las::CSR *)sprs)->getNumNonzero();
+  int nrows = ((las::CSR *)sprs)->getNumRows();
+  las::SparskitBuffers bfrs = las::SparskitBuffers(nrows, nnz * 100);
+  las::Solve * slv = createSparskitLUSolve(&bfrs, 1E-6);
+#elif defined USING_PETSC
+  las::Solve * slv = las::createPetscLUSolve(LAS_COMM_WORLD);
+#endif
+  slv->solve(m, u, v);
+  double * rslt = nullptr;
+  ops->get(u, 3, &rws[0], &rslt);
+  std::cout << "results" << std::endl;
+  for (int i = 0; i < 3; ++i)
+  {
+    std::cout << rslt[i] << " ";
+  }
+  std::cout << std::endl;
+  delete[] rslt;
+  mb->destroy(m);
+  vb->destroy(u);
+  vb->destroy(v);
+  delete slv;
+  delete mb;
+  delete vb;
+  delete ops;
+#if BACKEND == sparskit
+  las::destroySparsity<las::CSR *>(sprs);
+#endif
+#ifdef USING_PETSC
+  las::finalizePETScLAS();
+#endif
+  MPI_Finalize();
+}
